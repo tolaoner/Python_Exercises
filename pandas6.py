6a972faec1ff2c860cedcc81dce291b862ef0043
tolaoner <59030417+tolaoner@users.noreply.github.com>
2020-05-17 05:24:32 +0200

    more exercises

--- pandas6.py
+++ pandas6.py
@@ -1,1 +1,33 @@
-#exercise 12
+#exercise 12,13,14,15,16,17
+import numpy as np
+import pandas as pd
+'''12-----ser = pd.Series(np.random.randint(1, 10, 35))
+ser_df=pd.DataFrame(ser.values.reshape(7,5))
+print(ser_df)'''
+'''13----ser = pd.Series(np.random.randint(1, 10, 7))
+print(ser)
+a=np.argwhere(ser % 3==0)
+print(a)'''
+'''ser = pd.Series(list('abcdefghijklmnopqrstuvwxyz'))
+pos = [0, 4, 8, 14, 20]
+a=ser.take(pos)
+print(a)'''
+'''15---ser1 = pd.Series(range(5))
+ser2 = pd.Series(list('abcde'))
+a=ser1.append(ser2)#stack series vertically
+print(a)
+df=pd.concat([ser1,ser2],axis=1)
+print(df)'''
+'''16---ser1 = pd.Series([10, 9, 6, 5, 3, 1, 12, 8, 13])
+ser2 = pd.Series([1, 3, 10, 13])
+a= [pd.Index(ser1).get_loc(i) for i in ser2]
+print(a)'''
+'''---17 truth = pd.Series(range(10))
+pred = pd.Series(range(10)) + np.random.random(10)
+a=np.mean((truth-pred)**2)
+print(a)'''
+ser = pd.Series(['how', 'to', 'kick', 'ass?'])
+ser1=pd.Series([i[0].upper()+i[1:] for i in ser])
+a=ser.map(lambda x: x.title())
+print(ser1)
+print(a)
---
+++ pandas7.py
@@ -0,0 +1,10 @@
+#exercise 19,20,21,22
+import numpy as np
+import pandas as pd
+'''---19ser = pd.Series(['how', 'to', 'kick', 'ass?'])
+ser1= pd.Series([len(i) for i in ser])
+print(ser1)'''
+'''---20ser = pd.Series([1, 3, 6, 10, 15, 21, 27, 35])
+print(ser.diff())'''
+'''---21ser = pd.Series(['01 Jan 2010', '02-02-2011', '20120303', '2013/04/04', '2014-05-05', '2015-06-06T12:20'])
+print(pd.to_datetime(ser))'''
